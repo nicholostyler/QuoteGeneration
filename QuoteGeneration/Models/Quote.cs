3dbd13af8d803a3605c121bcef7f9aff5613cc98
Nicholos Tyler <nicholos.tyler@outlook.com>
2020-09-17 14:01:06 -0400

    First commit

---
+++ QuoteGeneration/Models/Quote.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace QuoteGeneration.Models
+{
+    public class Quote
+    {
+        public string _id { get; set; }
+        public List<string> tags { get; set; }
+        public string content { get; set; }
+        public string author { get; set; }
+        public int length { get; set; }
+    }
+}
